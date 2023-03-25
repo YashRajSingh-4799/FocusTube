b8044455538a53f704b33cf7da0efee4c8ebfdd9
YashRajSingh-4799 <yashrajsingh4799@gmail.com>
2023-03-25 22:46:20 +0530

    first commit

---
+++ src/utils/constants.js
@@ -0,0 +1,31 @@
+import CodeIcon from '@mui/icons-material/Code';
+import ComputerIcon from '@mui/icons-material/Computer';
+import LanguageIcon from '@mui/icons-material/Language';
+import CalculateIcon from '@mui/icons-material/Calculate';
+import SchoolIcon from '@mui/icons-material/School';
+import ScienceIcon from '@mui/icons-material/Science';
+import BiotechIcon from '@mui/icons-material/Biotech';
+import MonetizationOnIcon from '@mui/icons-material/MonetizationOn';
+import FollowTheSignsIcon from '@mui/icons-material/FollowTheSigns';
+
+export const logo = 'https://i.ibb.co/s9Qys2j/logo.png';
+
+export const categories = [
+  { name: 'Education', icon: <SchoolIcon />, },
+  { name: 'Coding', icon: <CodeIcon />, },
+  { name: 'English', icon: <LanguageIcon />, },
+  {name:'Computer Science',icon:<ComputerIcon/>},
+  { name: 'Math', icon: <CalculateIcon />, },
+  { name: 'Chemistry', icon: <ScienceIcon />, },
+  { name: 'Biology', icon: <BiotechIcon/>, },
+  { name: 'Commerce', icon: <MonetizationOnIcon/>, },
+  { name: 'Motivation', icon: <FollowTheSignsIcon/>, },
+  
+];
+
+export const demoThumbnailUrl = 'https://i.ibb.co/G2L2Gwp/API-Course.png';
+export const demoChannelUrl = '/channel/UCMXMp3Lc6v6v8dJH5ZGwtqA';
+export const demoVideoUrl = '/video/uykOHAhId0Q';
+export const demoChannelTitle = 'Raftaar';
+export const demoVideoTitle = 'SHEIKH CHILLI | RAFTAAR ( YEH DISS GAANA NAHI HAI )';
+export const demoProfilePicture = 'https://i.ibb.co/s9Qys2j/logo.png'
