6c528c573dd3c4e7e6a28882bbd7d35328120b30
Harshvardhan Singh Jadon <jadonharsh109@gmail.com>
2023-04-12 23:31:00 +0530

    Dockerfile added

--- src/utils/fetchFromAPI.js
+++ src/utils/fetchFromAPI.js
@@ -7,12 +7,12 @@ const options = {
     maxResults: 50,
   },
   url: 'https://youtube-v31.p.rapidapi.com/captions',
-  
+
   headers: {
-    'X-RapidAPI-Key': 'process.env.REACT_APP_API_KEY',
+    'X-RapidAPI-Key': '0e775d38damsh312b38c4f07187ep17a05ejsn994883147928',
     'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
   }
-}; 
+};
 
 export const fetchFromAPI = async (url) => {
   const { data } = await axios.get(`${BASE_URL}/${url}`, options);
